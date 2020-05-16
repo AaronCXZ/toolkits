92e2c9826453e304be2304db7fc9d6bc54a2a526
muskchen <284236865@qq.com>
2020-05-16 14:37:52 +0800

    添加sha256和sha512加密方法

--- str/md5.go
+++ str/md5.go
@@ -2,12 +2,31 @@ package str
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 	"io"
 )
 
 func MD5(str string) (string, error) {
 	h := md5.New()
+	if _, err := io.WriteString(h, str); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func Sha512(str string) (string, error) {
+	h := sha512.New()
+	if _, err := io.WriteString(h, str); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func Sha256(str string) (string, error) {
+	h := sha256.New()
+
 	if _, err := io.WriteString(h, str); err != nil {
 		return "", err
 	}
--- str/md5_test.go
+++ str/md5_test.go
@@ -7,10 +7,27 @@ import (
 
 func TestMD5(t *testing.T) {
 	s := "abc.1234%"
-	md5, err := MD5(s)
-	if err != nil {
-		t.Error(err)
-	}
-	fmt.Println(md5)
+	t.Run("md5", func(t *testing.T) {
+		md5, err := MD5(s)
+		if err != nil {
+			t.Error(err)
+		}
+		fmt.Println(md5)
+	})
+	t.Run("sha512", func(t *testing.T) {
+		sha, err := Sha512(s)
+		if err != nil {
+			t.Error(err)
+		}
+		fmt.Println(sha)
+	})
 
+	t.Run("sha256", func(t *testing.T) {
+		sha, err := Sha256(s)
+		if err != nil {
+			t.Error(err)
+		}
+		fmt.Println(sha)
+
+	})
 }
