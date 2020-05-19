c23835a1cae4464b59cf46db0685057cc425d572
Muskchen <284236865@qq.com>
2020-05-19 13:43:49 +0800

    增加使用zap的gin中间件

--- go.mod
+++ go.mod
@@ -5,9 +5,10 @@ go 1.14
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
-	github.com/stretchr/testify v1.5.1
+	github.com/stretchr/testify v1.4.0
+	go.uber.org/zap v1.15.0
 	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
-	gopkg.in/yaml.v2 v2.3.0
+	gopkg.in/yaml.v2 v2.2.8
 )
--- http/middleware/logger.go
+++ http/middleware/logger.go
@@ -1,11 +1,20 @@
 package middleware
 
 import (
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"runtime/debug"
+	"strings"
 	"time"
 
 	"github.com/Muskchen/toolkits/logger"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
+
+const TimeFormat = "2006-01-02 15:04:05"
 
 func ErrorLogger() gin.HandlerFunc {
 	return ErrorLoggerT(gin.ErrorTypeAny)
@@ -34,5 +43,107 @@ func LoggerMiddleware() gin.HandlerFunc {
 		logger.Infof("code: %3d latencyTime: %d clientIP: %s method: %s requestUrl: %s",
 			code, latencyTime, clientIP, method, path,
 		)
+	}
+}
+
+func GinZapLogger(log *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next()
+		end := time.Now()
+		latency := time.Since(start)
+
+		if len(c.Errors) > 0 {
+			for _, e := range c.Errors.Errors() {
+				log.Error(e)
+			}
+		} else {
+			log.Info(path,
+				zap.Int("status", c.Writer.Status()),
+				zap.String("method", c.Request.Method),
+				zap.String("path", path),
+				zap.String("query", query),
+				zap.String("ip", c.ClientIP()),
+				zap.String("user-agent", c.Request.UserAgent()),
+				zap.String("time", end.Format(TimeFormat)),
+				zap.Duration("latency", latency),
+			)
+		}
+	}
+}
+
+func GinZapSugaredLogger(log *zap.SugaredLogger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next()
+		end := time.Now()
+		latency := time.Since(start)
+
+		if len(c.Errors) > 0 {
+			for _, e := range c.Errors.Errors() {
+				log.Error(e)
+			}
+		} else {
+			log.Info(path,
+				zap.Int("status", c.Writer.Status()),
+				zap.String("method", c.Request.Method),
+				zap.String("path", path),
+				zap.String("query", query),
+				zap.String("ip", c.ClientIP()),
+				zap.String("user-agent", c.Request.UserAgent()),
+				zap.String("time", end.Format(TimeFormat)),
+				zap.Duration("latency", latency),
+			)
+		}
+	}
+}
+
+func RecoverWithZapLogger(log *zap.Logger, stack bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				var brokenPipe bool
+				if ne, ok := err.(*net.OpError); ok {
+					if se, ok := ne.Err.(*os.SyscallError); ok {
+						if strings.Contains(strings.ToLower(se.Error()), "broken pipe") ||
+							strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
+				}
+
+				httpRequest, _ := httputil.DumpRequest(c.Request, false)
+				if brokenPipe {
+					log.Error(c.Request.URL.Path,
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+					_ = c.Error(err.(error))
+					c.Abort()
+					return
+				}
+
+				if stack {
+					log.Error("[Recovery from panic]",
+						zap.Time("time", time.Now()),
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+						zap.String("stack", string(debug.Stack())),
+					)
+				} else {
+					log.Error("[Recovery from panic]",
+						zap.Time("time", time.Now()),
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+				}
+				c.AbortWithStatus(http.StatusInternalServerError)
+			}
+		}()
+		c.Next()
 	}
 }
