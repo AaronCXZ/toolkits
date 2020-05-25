c53eac71e392fa83adf55cd70b4d7d8bc90103ff
Muskchen <284236865@qq.com>
2020-05-25 16:57:49 +0800

    修改jwt认证

--- http/middleware/jwt/jwt.go
+++ http/middleware/jwt/jwt.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/Muskchen/toolkits/http/render"
-
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
--- http/middleware/jwt/jwt_test.go
+++ http/middleware/jwt/jwt_test.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
-
 	"github.com/stretchr/testify/assert"
 )
 
@@ -55,7 +54,7 @@ var jt *JWT = &JWT{SingingKey: []byte("test")}
 
 var foreverClaims CustomClaims = CustomClaims{
 	1000,
-	"defaul",
+	"default",
 	"default@qq.com",
 	jwt.StandardClaims{
 		ExpiresAt: 0,
