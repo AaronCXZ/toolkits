48158001ec68e43a494c492e262036819e6eab90
Muskchen <284236865@qq.com>
2020-07-09 16:50:31 +0800

    使用新的logs库

--- http/http.go
+++ http/http.go
@@ -6,12 +6,8 @@ import (
 	"time"
 
 	"github.com/Muskchen/toolkits/logs"
-
 	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
 )
-
-var logger *zap.SugaredLogger
 
 var srv = &http.Server{
 	ReadTimeout:    time.Duration(10) * time.Second,
@@ -20,28 +16,26 @@ var srv = &http.Server{
 }
 
 func Start(r *gin.Engine, addr string) {
-	logger = logs.GetSLogger()
 	srv.Addr = addr
 	srv.Handler = r
 	go func() {
-		logger.Infof("starting http server, listening on: %s", srv.Addr)
+		logs.Infof("starting http server, listening on: %s", srv.Addr)
 		if err := srv.ListenAndServe(); err != nil {
-			logger.Errorf("listening %s occur error: %s", srv.Addr, err)
+			logs.Errorf("listening %s occur error: %s", srv.Addr, err)
 		}
 	}()
 }
 
 func Shutdown() {
-	logger = logs.GetSLogger()
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(5)*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		logger.Errorf("cannot shutdown http server: %s", err)
+		logs.Errorf("cannot shutdown http server: %s", err)
 	}
 	select {
 	case <-ctx.Done():
-		logger.Info("shutdown http server timeout of 5 seconds.")
+		logs.Info("shutdown http server timeout of 5 seconds.")
 	default:
-		logger.Info("http server stopped")
+		logs.Info("http server stopped")
 	}
 }
