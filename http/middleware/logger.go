b405b9ea69e3523098cce84860b2008eb97495b8
Muskchen <284236865@qq.com>
2020-07-09 16:48:08 +0800

    使用新的logs库

--- http/middleware/logger.go
+++ http/middleware/logger.go
@@ -18,7 +18,6 @@ const TimeFormat = "2006-01-02 15:04:05"
 
 // 使用zao.logger的中间件
 func GinZapLogger() gin.HandlerFunc {
-	logger := logs.GetLogger()
 	return func(c *gin.Context) {
 		start := time.Now()
 		path := c.Request.URL.Path
@@ -29,40 +28,10 @@ func GinZapLogger() gin.HandlerFunc {
 
 		if len(c.Errors) > 0 {
 			for _, e := range c.Errors.Errors() {
-				logger.Error(e)
+				logs.Error(e)
 			}
 		} else {
-			logger.Info(path,
-				zap.Int("status", c.Writer.Status()),
-				zap.String("method", c.Request.Method),
-				zap.String("path", path),
-				zap.String("query", query),
-				zap.String("ip", c.ClientIP()),
-				zap.String("user-agent", c.Request.UserAgent()),
-				zap.String("time", end.Format(TimeFormat)),
-				zap.Duration("latency", latency),
-			)
-		}
-	}
-}
-
-// 使用zap.SugaredLogger的中间件
-func GinZapSugaredLogger() gin.HandlerFunc {
-	logger := logs.GetSLogger()
-	return func(c *gin.Context) {
-		start := time.Now()
-		path := c.Request.URL.Path
-		query := c.Request.URL.RawQuery
-		c.Next()
-		end := time.Now()
-		latency := time.Since(start)
-
-		if len(c.Errors) > 0 {
-			for _, e := range c.Errors.Errors() {
-				logger.Error(e)
-			}
-		} else {
-			logger.Info(path,
+			logs.Info(path,
 				zap.Int("status", c.Writer.Status()),
 				zap.String("method", c.Request.Method),
 				zap.String("path", path),
@@ -78,7 +47,6 @@ func GinZapSugaredLogger() gin.HandlerFunc {
 
 // Panic恢复的中间件
 func RecoverWithZapLogger(stack bool) gin.HandlerFunc {
-	logger := logs.GetSLogger()
 	return func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
@@ -94,7 +62,7 @@ func RecoverWithZapLogger(stack bool) gin.HandlerFunc {
 
 				httpRequest, _ := httputil.DumpRequest(c.Request, false)
 				if brokenPipe {
-					logger.Error(c.Request.URL.Path,
+					logs.Error(c.Request.URL.Path,
 						zap.Any("error", err),
 						zap.String("request", string(httpRequest)),
 					)
@@ -104,14 +72,14 @@ func RecoverWithZapLogger(stack bool) gin.HandlerFunc {
 				}
 
 				if stack {
-					logger.Error("[Recovery from panic]",
+					logs.Error("[Recovery from panic]",
 						zap.Time("time", time.Now()),
 						zap.Any("error", err),
 						zap.String("request", string(httpRequest)),
 						zap.String("stack", string(debug.Stack())),
 					)
 				} else {
-					logger.Error("[Recovery from panic]",
+					logs.Error("[Recovery from panic]",
 						zap.Time("time", time.Now()),
 						zap.Any("error", err),
 						zap.String("request", string(httpRequest)),
