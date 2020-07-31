4a14cb51bdfefffa0f33bddf5b1cde73ea71240b
Muskchen <284236865@qq.com>
2020-07-31 13:59:26 +0800

    状态模式

---
+++ design-pattern/16-state/state_test.go
@@ -0,0 +1,18 @@
+package state
+
+import "testing"
+
+func ExampleWeek() {
+	ctx := NewDayContext()
+	todayAndNext := func() {
+		ctx.Today()
+		ctx.Next(ctx)
+	}
+	for i := 0; i < 8; i++ {
+		todayAndNext()
+	}
+}
+
+func TestWeek(t *testing.T) {
+	ExampleWeek()
+}
